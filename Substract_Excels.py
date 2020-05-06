f9333a3dc6a829f9cf492a02cf2a411a716bef8e
sergi <s_ponsa_cobas@hotmail.com>
2020-05-06 14:29:26 +0200

    Substract training to Original Mujoco

--- Substract_Excels.py
+++ Substract_Excels.py
@@ -7,9 +7,9 @@ from Experiments_pybullet import SubstractExcel_2_excel
 
 if __name__ == '__main__':
 
-    path_Excel_Mujoco = "/home/sergi/GitHub/Mujoco_TFM/Paper_resampled/Single_6_averageconverted.xlsx"
-    path_Excel_Pybullet = "/home/sergi/GitHub/Simulation_Pybullet/Paper_Experiments/original_Single_6_average.xlsx"
-
-    SubstractExcel_2_excel(path_Excel_Pybullet,path_Excel_Mujoco,title = 'diff_from_mujoco_6',folder = "Paper_sub")
+    path_Excel_Mujoco = "/home/sergi/GitHub/Parameters_CrossEntrophy_TFM/Original_Mujoco_Training1_averageconverted.xlsx"
+    #path_Excel_Pybullet = "/home/sergi/GitHub/Parameters_CrossEntrophy_TFM/Train_parameters_result.xlsx"
+    path_Excel_Pybullet ="/home/sergi/GitHub/Simulation_Pybullet/Experiments_Training1/Original_average.xlsx"
+    SubstractExcel_2_excel(path_Excel_Pybullet,path_Excel_Mujoco,title = 'diff_from_mujoco_NoTrained',folder = "Paper_sub")
 
     print ("Done")
